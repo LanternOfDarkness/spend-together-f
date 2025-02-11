974e7b419443e6f22c5762f2006e3792b7bc350f
lanternofdarkness <kinlo443@gmail.com>
2025-02-11 23:29:20 +0200

    Created WelcomePage file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage/LoginPage';
+import WelcomePage from './pages/WelcomePage/WelcomePage';
 
 const App: React.FC = () => {
   return (
     <Router>
       <div className="App">
         <Routes>
+          <Route path="/" element={<WelcomePage />} />
           <Route path="/login" element={<LoginPage />} />
         </Routes>
       </div>
---
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './WelcomePage.scss';
+
+const WelcomePage: React.FC = () => {
+  return <div className="welcome-page">WelcomePage</div>;
+};
+
+export default WelcomePage;
