c76825f284e77d84645c34499e04b9da552645c5
lanternofdarkness <kinlo443@gmail.com>
2025-02-11 22:58:08 +0200

    Reconfigure project with TypeScript support and improved folder structure

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "./logo.svg";
+
+const App: React.FC = () => {
+    return <div>
+      <img src={logo} className="App-logo" alt="logo" width={200} />
+      <h1>Hello JSX with TypeScript!</h1></div>;
+
+};
+
+export default App;
