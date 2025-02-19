ffbf34da8a6e59606af9f9508ecc7c5038e3687e
lanternofdarkness <kinlo443@gmail.com>
2025-02-19 23:04:40 +0200

    Added profile element. Added box for links to other pages.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,27 @@
 // src/components/Header/Header.tsx
 
 import React, { useState, useEffect } from 'react';
-import { Box, AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { Box, AppBar, Toolbar, Typography, Button, IconButton, Menu, MenuItem } from '@mui/material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { Link } from 'react-router-dom';
 import './Header.scss';
 
 const Header: React.FC = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false); 
+  const [isLoggedIn, setIsLoggedIn] = useState(true); 
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
-  useEffect(() => {
-    const user = localStorage.getItem('user');
-    setIsLoggedIn(!!user);
-  }, []);
+  // useEffect(() => {
+  //   const user = localStorage.getItem('user');
+  //   setIsLoggedIn(!!user);
+  // }, []);
+
+  const handleProfileClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
 
   return (  
     <Box className="header-container">
@@ -31,9 +40,34 @@ const Header: React.FC = () => {
               </Typography>
             </Link>
           </Box>
+          <Box className="links-container">
+            
+          </Box>
           <Box className="buttons-container">
-            <Button color="inherit" sx={{ color: 'black' }}>Login</Button>
-            <Button color="primary" sx={{ ml: 2 }}>Sign Up</Button>
+          {!isLoggedIn ? (
+              <>
+                <Button sx={{color: 'text.primary'}}>Login</Button>
+                <Button sx={{color: 'text.primary'}}>Sign Up</Button>
+              </>
+            ) : (
+              <>
+                <IconButton color="inherit" onClick={handleProfileClick}>
+                  <AccountCircleIcon />
+                </IconButton>
+                <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
+                  <MenuItem onClick={handleMenuClose}>
+                    <Link to="/profile" style={{ textDecoration: 'none', color: 'inherit' }}>
+                      Profile
+                    </Link>
+                  </MenuItem>
+                  <MenuItem onClick={handleMenuClose}>
+                    <Link to="/logout" style={{ textDecoration: 'none', color: 'inherit' }}>
+                      Logout
+                    </Link>
+                  </MenuItem>
+                </Menu>
+              </>
+            )}
           </Box>
         </Toolbar>
       </AppBar>
